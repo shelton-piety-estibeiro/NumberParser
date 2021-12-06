47f6668448a40314ae1b4c012a071e8b8a18e260
shelton-piety-estibeiro <shelton.estibeiro@gmail.com>
2021-12-06 22:40:39 +0530

    initial commit

--- NumberParser/Program.cs
+++ NumberParser/Program.cs
@@ -6,7 +6,33 @@ namespace NumberParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputString;
+            Console.WriteLine("Enter numbers and File format");
+            inputString = Console.ReadLine();
+            string numbers = inputString.Split(' ')[0];
+            string fileFormat = inputString.Split(' ')[inputString.Split(' ').Length - 1];
+            Console.Write(SortNumberByDescendingOrder(numbers) + ' ' + fileFormat);
+            Console.ReadLine();
+
+        }
+
+        private static string SortNumberByDescendingOrder(string numbers)
+        {
+            string[] intArray = numbers.Split(',');
+            for (int i=0; i<intArray.Length - 1; i++)
+            {
+                for(int j=i+1; j<intArray.Length; j++)
+                {
+                    if(Convert.ToInt32(intArray[i]) < Convert.ToInt32(intArray[j]))
+                    {
+                    int temp = Convert.ToInt32(intArray[i]);
+                    intArray[i] = intArray[j];
+                    intArray[j] = temp.ToString();
+
+                    }
+                }
+            }
+            return string.Join(",", intArray);
         }
     }
 }
